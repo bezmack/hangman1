3d505b724157b3f8d11830b40bb7a68c12de5998
Hernan Tejada <hernan.tejada@gmail.com>
2017-06-06 22:35:00 -0400

    javascript updated

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,18 +2,27 @@
 
 var car=["TOYOTA", "HONDA", "BMW", "MERCEDES", "NISSAN"];
 var computerPick = car[Math.floor(Math.random() * car.length)];
-console.log (computerPick);
 var splitCar = computerPick.split("");
-console.log (splitCar);
 var currentWord = new Array (splitCar.length);
+for (var i=0; i<currentWord.length; i++) {
+	currentWord[i] = "_ ";
+}
 
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 	var processWord = userGuess.toUpperCase();
-	for (i=0; i<splitCar.length; i++) {
+	for (var i = 0; i < splitCar.length; i++) {
 		if (splitCar[i] === processWord) {
 			currentWord[i] = splitCar[i];
 			console.log(currentWord);
+			var cword = document.getElementById ("cword");
+			cword.innerHTML = "";
+			for (var j = 0; j < currentWord.length; j++) {
+				cword = document.getElementById("cword");
+				var cwordUpdate = document.createTextNode(currentWord[j]);
+				cword.appendChild(cwordUpdate);
+			}
 		}
 	}
 };
+
